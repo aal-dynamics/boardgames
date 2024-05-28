c3785f80ec71ae41106d086def0ad63391648d09
aal-dynamics <a.alferin@dynamicsgroup.de>
2024-05-28 15:42:12 +0200

    go

--- .github/DeployToOnPrem.ps1
+++ .github/DeployToOnPrem.ps1
@@ -29,9 +29,9 @@ Write-Host "Install or update Apps"
 # execute Install CmdLet
 Start-Sleep -Seconds 3.3
 $bgApp = Get-ChildItem -Path $deployAppsPath -Filter "mephezar_boardgames_*.app"
-if ($bgApp) {
+#if ($bgApp) {
     Publish-BCContainerApp -containerName $containerName -appFile $bgApp -skipVerification -sync -install
-}
+#}
 
 Start-Sleep -Seconds 3.3
 $filename = $date + "_99_finish.txt"
